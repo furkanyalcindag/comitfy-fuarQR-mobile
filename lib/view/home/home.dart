c955c4f90e1bb3d71528c02ac9fc7824e337a411
Yunus Yıldız <yunusyld7@gmail.com>
2022-12-30 10:05:23 +0300

    Bottom buttons are relative to screen in portrait mode

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -9,6 +9,8 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/widgets.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:fuar_qr/core/services/article/article_manager.dart';
+import 'package:fuar_qr/core/utility/constants.dart';
+import 'package:fuar_qr/core/utility/theme_choice.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class Home extends StatefulWidget {
@@ -41,84 +43,105 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: Stack(
         children: <Widget>[
+          // Dont put any widget on top of the QR Code scanner
           _buildQrView(context),
-          FittedBox(
-            fit: BoxFit.contain,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                if (_articleData != null)
-                  Text('Title: ${_articleData!['title']}')
-                else
-                  const Text('Scan a code'),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.all(8),
-                      child: ElevatedButton(
-                          onPressed: () async {
-                            await controller?.toggleFlash();
-                            setState(() {
-                              _scannerBorderColor = Colors.green;
-                            });
-                          },
-                          child: FutureBuilder(
-                            future: controller?.getFlashStatus(),
-                            builder: (context, snapshot) {
-                              return Text('Flash: ${snapshot.data}');
-                            },
-                          )),
+          Positioned(
+            top: 0,
+            child: FittedBox(
+              fit: BoxFit.contain,
+              child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: ThemeChoice()),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            width: MediaQuery.of(context).size.width,
+            child: FittedBox(
+              fit: BoxFit.contain,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  if (_articleData != null)
+                    Text('Title: ${_articleData!['title']}')
+                  else
+                    Text(
+                      'Scan a code',
+                      style: Theme.of(context).textTheme.bodyLarge!.merge(
+                            TextStyle(color: white),
+                          ),
                     ),
-                    Container(
-                      margin: const EdgeInsets.all(8),
-                      child: ElevatedButton(
-                          onPressed: () async {
-                            await controller?.flipCamera();
-                            setState(() {});
-                          },
-                          child: FutureBuilder(
-                            future: controller?.getCameraInfo(),
-                            builder: (context, snapshot) {
-                              if (snapshot.data != null) {
-                                return Text(
-                                    'Camera facing ${describeEnum(snapshot.data!)}');
-                              } else {
-                                return const Text('loading');
-                              }
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                            onPressed: () async {
+                              await controller?.toggleFlash();
+                              setState(() {
+                                _scannerBorderColor = Colors.green;
+                              });
                             },
-                          )),
-                    )
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.all(8),
-                      child: ElevatedButton(
-                        onPressed: () async {
-                          await controller?.pauseCamera();
-                        },
-                        child:
-                            const Text('pause', style: TextStyle(fontSize: 20)),
+                            child: FutureBuilder(
+                              future: controller?.getFlashStatus(),
+                              builder: (context, snapshot) {
+                                return Text('Flash: ${snapshot.data}');
+                              },
+                            )),
                       ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.all(8),
-                      child: ElevatedButton(
-                        onPressed: () async {
-                          await controller?.resumeCamera();
-                        },
-                        child: const Text('resume',
-                            style: TextStyle(fontSize: 20)),
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                            onPressed: () async {
+                              await controller?.flipCamera();
+                              setState(() {});
+                            },
+                            child: FutureBuilder(
+                              future: controller?.getCameraInfo(),
+                              builder: (context, snapshot) {
+                                if (snapshot.data != null) {
+                                  return Text(
+                                      'Camera facing ${describeEnum(snapshot.data!)}');
+                                } else {
+                                  return const Text('loading');
+                                }
+                              },
+                            )),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            await controller?.pauseCamera();
+                          },
+                          child: const Text('pause',
+                              style: TextStyle(fontSize: 20)),
+                        ),
                       ),
-                    )
-                  ],
-                ),
-              ],
+                      Container(
+                        margin: const EdgeInsets.all(8),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            await controller?.resumeCamera();
+                          },
+                          child: const Text(
+                            'resume',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           )
         ],
@@ -157,7 +180,7 @@ class _HomeState extends State<Home> {
     setState(() {
       this.controller = controller;
     });
-    controller.resumeCamera();
+    controller.pauseCamera();
     controller.scannedDataStream.listen((scanData) async {
       if (result?.code != scanData.code) {
         _getArticleTitle(uuid: scanData.code);
